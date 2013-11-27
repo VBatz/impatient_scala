61d3a722e80556627cafcf7a2bec957e2a0f4a05
VBatz <veronika.batz@gutefrage.net>
2013-11-27 14:42:28 +0100

    exercises chapter 3: 9

---
+++ src/main/scala/chapter_3_9.scala
@@ -0,0 +1,9 @@
+import scala.collection.mutable.ArrayBuffer
+
+val in = java.util.TimeZone.getAvailableIDs
+
+val out = ArrayBuffer[String]()
+
+for ( i <- 0 until in.length if in(i).startsWith("America")) yield out += in(i).drop(8)
+
+println("Time zones starting with America: " + out.mkString("(", ", ", ")") + "\n")
