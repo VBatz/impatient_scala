d983105f290f94b12103575160df8ceb4930d49b
VBatz <veronika.batz@gutefrage.net>
2013-11-27 09:08:56 +0100

    exercises chapter 3: 2 and 3

--- src/main/scala/chapter_3_2.scala
+++ src/main/scala/chapter_3_2.scala
@@ -8,16 +8,12 @@ for (i <- 0 until a.length) {
   a(i) = i
 }
 
-val indexes = for (i <- 0 until a.length) yield {
-  if (i % 2 == 0) {
-    if (i == a.length - 1) i
-    else i+1
-  }
-  else i - 1
+printf("\nArray a with %d elements: " + a.mkString("(", ", ", ")") + "\n", n)
+
+for (i <- 0 until a.length if i % 2 == 1) {
+  val x = a(i)
+  a(i) = a(i-1)
+  a(i-1) = x
 }
 
-val b = new Array[Int](a.length)
-
-for (j <- 0 until indexes.length) b(j) = a(indexes(j))
-
-println(b.mkString(", "))
+println("\nSwapped adjacent elements of Array a: " + a.mkString("(", ", ", ")") + "\n")
