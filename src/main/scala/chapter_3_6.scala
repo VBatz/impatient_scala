fdb2e8ff34f3adc370420434197b6c27a5f53dd8
VBatz <veronika.batz@gutefrage.net>
2013-11-27 13:17:44 +0100

    exercises chapter 3: 6 und 7

--- src/main/scala/chapter_3_6.scala
+++ src/main/scala/chapter_3_6.scala
@@ -2,10 +2,12 @@ import scala.collection.mutable.ArrayBuffer
 
 val a = Array(-2,1,5,-8,-1,0,6,-3,2,1,4,6,8,-2)
 
-a.reverse
+println("\nOriginal array: " + a.mkString("(", ", ", ")"))
+
+println("\nArray: " + a.reverse.mkString("(", ", ", ")"))
 
 val b = ArrayBuffer[Int]()
 
 b ++= a
 
-b.reverse
+println("\nArrayBuffer: " + b.reverse.mkString("(", ", ", ")") + "\n")
