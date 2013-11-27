73c551ca93c449394564bfa16adb450f463b67dc
VBatz <veronika.batz@gutefrage.net>
2013-11-27 14:52:29 +0100

    exercises chapter 3: 9 Test Github

--- src/main/scala/chapter_3_9.scala
+++ src/main/scala/chapter_3_9.scala
@@ -7,4 +7,3 @@ val out = ArrayBuffer[String]()
 for ( i <- 0 until in.length if in(i).startsWith("America")) yield out += in(i).drop(8)
 
 println("Time zones starting with America: " + out.mkString("(", ", ", ")") + "\n")
-println("Test für History in Githawöourhtatiuwhrub ")
