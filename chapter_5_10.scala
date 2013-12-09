f7a13317db30e64646e2e339b7dd45b4cd5cd162
VBatz <veronika.batz@gutefrage.net>
2013-12-09 13:33:02 +0100

    chapter 5: exercise 10

--- chapter_5_10.scala
+++ chapter_5_10.scala
@@ -1,4 +1,13 @@
 
+class EmployeeExercise(val name: String, val salary: Double) {
+  def this() { this("John Q. Public", 0.0) }
+}
+
+// EmployeeExercise ist besser:
+// Weil Primary Constructor in nur einer Zeile beschreibt,
+// was Employee in 7 Zeilen macht.
+// Trotzdem blöd: hartkodierte Default-Werte
+
 class Employee {
   var name: String
   var salary: Double
