9e7817a8e10620ffda28291df20281d7150169e2
VBatz <veronika.batz@gutefrage.net>
2014-01-31 09:26:51 +0100

    first try chapter 9.10

--- chapter_9_10_serializable_Person.scala
+++ chapter_9_10_serializable_Person.scala
@@ -1,19 +1,41 @@
 import scala.collection.mutable.ArrayBuffer
 
-class Person extends Serializable {
+class Person(val name: String) extends Serializable {
   private val friends = new ArrayBuffer[Person]
+
+  def getFriends(newFriend: Person) {
+    friends += newFriend
+  }
+
+  def currentFriends = {
+    println("Friends of " + this.name + ": " + friends.mkString(", "))
+  }
+
+  override def toString = this.name
 }
 
-val fred = new Person()
-val john = new Person()
-val jane = new Person()
-val june = new Person()
+val fred = new Person("Fred")
+val john = new Person("John")
+val jane = new Person("Jane")
+val june = new Person("June")
+
+fred.getFriends(john)
+
+john.getFriends(fred)
+john.getFriends(june)
+
+june.getFriends(john)
+june.getFriends(jane)
+
+john.currentFriends
+
+val allMyPersons = Array[Person](john, fred, jane, june)
 
 import java.io._
 
-val out = new ObjectOutputStream(new FileOutputStream("chapter_9_10.obj"))
-out.writeObject(fred)
-out.close()
+//val out = new ObjectOutputStream(new FileOutputStream("chapter_9_10.obj"))
+//out.writeObject(fred)
+//out.close()
 
-val in = new ObjectInputStream(new FileInputStream("chapter_9_10.obj"))
-val savedFred = in.readObject().asInstanceOf[Person]
+//val in = new ObjectInputStream(new FileInputStream("chapter_9_10.obj"))
+//val savedFred = in.readObject().asInstanceOf[Person]
