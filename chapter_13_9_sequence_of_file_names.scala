fe268e45bfb8e743aa992c1c4889d8f2b37eff11
VBatz <veronika.batz@gutefrage.net>
2014-02-17 09:58:09 +0100

    exercise 13-9: new thread for each file

--- chapter_13_9_sequence_of_file_names.scala
+++ chapter_13_9_sequence_of_file_names.scala
@@ -1,8 +1,20 @@
 
 import scala.io.Source
 val fileNames = Seq("chapter_9_3.txt", "chapter_9_6.txt", "chapter_9_7.txt")
+import scala.collection.mutable
+val frequencies = new mutable.HashMap[Char, Int] with scala.collection.mutable.SynchronizedMap[Char, Int]
 
+for (file <- fileNames) {
+  val contents = Source.fromFile(file).mkString
+  for (c <- contents) {
+    frequencies(c) = frequencies.getOrElse(c, 0) + 1
+  }
+}
 
+println(frequencies)
+println("Funktioniert - aber Seite 177: \"iterating over threadsafe collections is NOT safe and will likely lead to errors in your code\"")
+
+/*
 val source = Source.fromFile("chapter_9_3.txt")
 val contents = source.mkString
 
@@ -14,6 +26,8 @@ for (c <- contents) {
 }
 
 println(frequencies)
+*/
+
 
 /*
 import scala.collection.JavaConversions.asScalaConcurrentMap
